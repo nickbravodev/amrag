c146e756c83fd491bee7055450329939b55f30dd
Nick Bravo <nick@nickbravo.dev>
2024-01-13 20:19:04 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
@@ -8,8 +8,8 @@ import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
+  // output: "server",
+  // adapter: netlify(),
   site: "https://americanragnarok.com",
   integrations: [
     sitemap(),
