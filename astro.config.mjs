fa6111c6a6b9dd798cd7315af9741c595cbaa6e8
Nick Bravo <nick@nickbravo.dev>
2024-01-13 20:22:11 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-// import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
-
 import htmlBeautifier from "astro-html-beautifier";
+// import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
